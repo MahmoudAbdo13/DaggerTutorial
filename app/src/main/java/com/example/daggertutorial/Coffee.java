4165176d7018c87a1be90e482e55cac4f81b9cb1
Mahmoud Mohamed <mahmoudm.abdelwadood@gmail.com>
2022-12-21 14:29:55 +0200

    used constructor, field and method injection

--- app/src/main/java/com/example/daggertutorial/Coffee.java
+++ app/src/main/java/com/example/daggertutorial/Coffee.java
@@ -5,15 +5,19 @@ import android.util.Log;
 import javax.inject.Inject;
 
 public class Coffee {
+
     private static final String TAG = "Coffee";
+
     @Inject
     Farm farm;
-    @Inject
+
     River river;
 
 
     @Inject
-    public Coffee() {
+    public Coffee(River river) {
+        this.river = river;
+        Log.e(TAG, "Coffee: ");
     }
 
     @Inject
--- app/src/main/java/com/example/daggertutorial/River.java
+++ app/src/main/java/com/example/daggertutorial/River.java
@@ -1,11 +1,17 @@
 package com.example.daggertutorial;
 
 
+import android.util.Log;
+
 import javax.inject.Inject;
 
 public class River {
+
+    private static final String TAG = "River";
+
     @Inject
     public River() {
+        Log.e(TAG, "River: ");
     }
 
     public String getWater(){
