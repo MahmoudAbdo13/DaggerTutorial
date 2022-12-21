a998258ab2fdac9d26f2355988c1ea904b40d156
Mahmoud Mohamed <mahmoudm.abdelwadood@gmail.com>
2022-12-21 12:54:54 +0200

    manual DI

--- app/src/main/java/com/example/daggertutorial/Coffee.java
+++ app/src/main/java/com/example/daggertutorial/Coffee.java
@@ -1,5 +1,11 @@
 package com.example.daggertutorial;
 
 public class Coffee {
+    private Farm farm;
+    private River river;
 
+    public Coffee(Farm farm, River river) {
+        this.farm = farm;
+        this.river = river;
+    }
 }
