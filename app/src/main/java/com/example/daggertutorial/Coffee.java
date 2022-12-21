fd50a46705ec5065e3e7f033a5ac149f783d96b9
Mahmoud Mohamed <mahmoudm.abdelwadood@gmail.com>
2022-12-21 14:24:24 +0200

    method injection

--- app/src/main/java/com/example/daggertutorial/Coffee.java
+++ app/src/main/java/com/example/daggertutorial/Coffee.java
@@ -1,15 +1,24 @@
 package com.example.daggertutorial;
 
+import android.util.Log;
+
 import javax.inject.Inject;
 
 public class Coffee {
+    private static final String TAG = "Coffee";
     @Inject
     Farm farm;
     @Inject
     River river;
 
+
     @Inject
     public Coffee() {
+    }
+
+    @Inject
+    public void connectElectricity(){
+        Log.e(TAG, "connectElectricity: Connect...");
     }
 
     public String getCoffeeCup(){
